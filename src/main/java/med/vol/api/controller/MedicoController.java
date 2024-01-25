4707a6017263f3d808ea825a49ddc9c5f41887ba
lgfas <luis.guilherme2811@gmail.com>
2024-01-25 12:01:41 -0300

    put e delete - 05.02

---
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -0,0 +1,39 @@
+package med.vol.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.vol.api.medico.DadosCadastroMedico;
+import med.vol.api.medico.DadosListagemMedico;
+import med.vol.api.medico.Medico;
+import med.vol.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        medicoRepository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosCadastroMedico dados) {
+
+    }
+
+}
---
+++ src/main/java/med/vol/api/controller/PacienteController.java
@@ -0,0 +1,34 @@
+package med.vol.api.controller;
+
+import jakarta.validation.Valid;
+import med.vol.api.paciente.DadosCadastroPaciente;
+import med.vol.api.paciente.DadosListagemPaciente;
+import med.vol.api.paciente.Paciente;
+import med.vol.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+    }
+}
