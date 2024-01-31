6405acf6df66984b40da7bf0c23ed02bd5f6dcee
lgfas <luis.guilherme2811@gmail.com>
2024-01-31 12:02:29 -0300

    curso 3 - documentar, testar e deploy - 01.03

---
+++ src/main/java/med/vol/api/domain/consulta/ConsultaRepository.java
@@ -0,0 +1,8 @@
+package med.voll.api.domain.consulta;
+
+import med.vol.api.domain.consulta.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+}
