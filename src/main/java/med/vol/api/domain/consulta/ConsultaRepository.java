06dfafb3989a853b50fafa0064543a5606dca152
lgfas <luis.guilherme2811@gmail.com>
2024-02-01 12:00:06 -0300

    curso 3 - documentar, testar e deploy - 02.02

--- src/main/java/med/vol/api/domain/consulta/ConsultaRepository.java
+++
@@ -1,8 +0,0 @@
-package med.voll.api.domain.consulta;
-
-import med.vol.api.domain.consulta.Consulta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
-
-}
--- src/main/java/med/vol/api/domain/consulta/DadosDetalhamentoConsulta.java
+++
@@ -1,6 +0,0 @@
-package med.vol.api.domain.consulta;
-
-import java.time.LocalDateTime;
-
-public record DadosDetalhamentoConsulta(Long id, Long idMedico, Long idPaciente, LocalDateTime data) {
-}
